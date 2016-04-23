7c7622b692b4252c6006621626c907d59b0c1e54
Dmitry.Stepanov <code@interplanet.link>
2016-04-23 18:12:06 +0300

    Do initial configuration

--- vagrant/provision/bootstrap.sh
+++ vagrant/provision/bootstrap.sh
@@ -15,6 +15,18 @@ php5enmod xdebug
 rm -r /tmp/xdebug
 
 echo "Installing RabbitMQ"
-apt-get install rabbitmq-server
+apt-get install rabbitmq-server -y
 rabbitmq-plugins enable rabbitmq_management
 service rabbitmq-server restart
+
+echo "Installing Composer"
+wget --no-verbose -P /tmp/composer/ https://getcomposer.org/installer
+php installer --install-dir=/usr/local/bin --filename=composer
+rm -r /tmp/composer
+
+echo "Composing vendor libraries"
+composer ~/projects/bot
+
+echo "Making the main script executable"
+chmod +x ~/projects/bot/bot
+
