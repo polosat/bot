77f8f5fddbded0d0cf6f5ac8b43d78e7f5df55e8
Dmitry.Stepanov <code@interplanet.link>
2016-04-23 14:41:30 +0300

    Preparing a new project

---
+++ vagrant/provision/bootstrap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo "Disabling locales acception from ssh clients"
+sed -i 's/\(^\s*AcceptEnv LANG LC_\*\s*$\)/#\1/' /etc/ssh/sshd_config
+service ssh restart
+
+echo "Installing PHP"
+apt-get install php5-common php5-cli -y
+
+echo "Installing xdebug"
+wget --no-verbose -P /tmp/xdebug/ https://bitbucket.org/polosat/packages/downloads/xdebug-2.3.2-ubuntu-trusty64.tar.bz2
+tar xjf /tmp/xdebug/xdebug-2.3.2-ubuntu-trusty64.tar.bz2 -C /tmp/xdebug
+install -m 644 -T /tmp/xdebug/xdebug-2.3.2.so /usr/lib/php5/20121212/xdebug.so
+echo -e '\nzend_extension = /usr/lib/php5/20121212/xdebug.so\n' >> /etc/php5/mods-available/xdebug.ini
+php5enmod xdebug
+rm -r /tmp/xdebug
+
+echo "Installing RabbitMQ"
+apt-get install rabbitmq-server
+rabbitmq-plugins enable rabbitmq_management
+service rabbitmq-server restart
